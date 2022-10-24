52a937f5fce775af9a5014a81c0254c316e8c3c3
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-24 18:14:45 +0300

    Add files via upload

---
+++ backend/src/models/card.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    validate: {
+      validator: (v) => /https?:\/\/(w{3})?[a-z0-9-]+\.[a-z0-9\S]{2,}/.test(v),
+    },
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'user',
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
