83ebbbec012964a301300549e83b4f99efe6b94e
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-11-15 20:35:34 +0300

    Delete backend directory

--- backend/src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'Сервер не работает' : message });
-  next();
-};
-
-module.exports = errorHandler;
